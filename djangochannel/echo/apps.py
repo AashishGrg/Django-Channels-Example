dfeb3cd701ad566dd82deffaac83165a287a3d0d
aashish <aashishgurung030@gmail.com>
2017-04-06 10:44:52 +0545

    working code

---
+++ djangochannel/echo/apps.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class EchoConfig(AppConfig):
+    name = 'echo'
+
+    def ready(self):
+        import echo.signals
